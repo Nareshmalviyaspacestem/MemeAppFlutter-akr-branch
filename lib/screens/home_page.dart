699ca135cdcfa627981bd7a4a0b569074160bf69
IntuzDevE17 <deve17@intuz.net>
2023-06-01 18:15:12 +0530

    add filter image image fill

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'package:dynamic_height_grid_view/dynamic_height_grid_view.dart';
 import 'package:memeapp/models/cart_card_model.dart';
 import 'package:memeapp/providers/cart_counter_provider.dart';
@@ -61,15 +62,18 @@ class _HomePageState extends State<HomePage> {
               children: [
                 IconButton(
                     icon: const Icon(
-                      Icons.shopping_cart_outlined,
+                      Icons.filter_list_rounded,
                       color: Colors.white,
                     ),
                     iconSize: 30,
                     onPressed: () {
-                      Navigator.push(context,
+
+                      /*Navigator.push(context,
                           MaterialPageRoute(builder: (context) => CartPage()));
+                      */
+
                     }),
-                Stack(
+              /*  Stack(
                   alignment: Alignment.center,
                   children: [
                     const CircleAvatar(
@@ -78,7 +82,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                     Text("$cartCounter"),
                   ],
-                ),
+                ),*/
               ],
             ),
           )
@@ -92,11 +96,11 @@ class _HomePageState extends State<HomePage> {
             thumbVisibility: true,
             thickness: 5,
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+              padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
               child: DynamicHeightGridView(
                   crossAxisCount: 2,
                   mainAxisSpacing: 5,
-                  crossAxisSpacing: 10,
+                  crossAxisSpacing: 5,
                   itemCount:
                       //nullcheck operator
                       snapshot.hasData ? snapshot.data!.data!.memes!.length : 1,
@@ -131,8 +135,13 @@ class _HomePageState extends State<HomePage> {
                                           fontWeight: FontWeight.w500)),
                                   const SizedBox(height: 5),*/
                                   ClipRRect(
-                                      borderRadius: BorderRadius.circular(8.0),
-                                      child: Image.network(memeImageUrl)),
+                                      borderRadius: BorderRadius.only(topLeft: Radius.circular(8.0) ,topRight:  Radius.circular(8.0)),
+                                      child: SizedBox(
+                                          height: 120,
+                                          width: double.infinity,
+                                          child: Image.network(memeImageUrl , width: double.infinity,height: double.infinity,fit: BoxFit.fill,)
+
+                                      )),
                                   const SizedBox(height: 5),
                                  Row(children: [
                                    IconButton(onPressed: (){
